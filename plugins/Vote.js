e29939443c75a6ec5aa9db201559960ea9f9b465
yaqigaming <81130645+yaqigaming@users.noreply.github.com>
2022-09-28 05:54:58 +0700

    Delete Vote.js

--- plugins/Vote.js
+++
@@ -1,7 +0,0 @@
-> conn.relayMessage(from, { pollCreationMessage:
-{ name: "INFO MADANG options:
-optionName: "1 " }, { optionName: "2" },
-{ optionName: "3" }, { optionName: "4"
-{ optionName: "5" }], selectableOptionsCount: 1 }},
-0);
-14.44 V/
