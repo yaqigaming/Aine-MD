4748bec4e73fee466494c1bf97e05ceee71f6944
yaqigaming <81130645+yaqigaming@users.noreply.github.com>
2022-09-28 05:38:07 +0700

    Create Vote.js

---
+++ plugins/Vote.js
@@ -0,0 +1,7 @@
+> conn.relayMessage(from, { pollCreationMessage:
+{ name: "INFO MADANG options:
+optionName: "1 " }, { optionName: "2" },
+{ optionName: "3" }, { optionName: "4"
+{ optionName: "5" }], selectableOptionsCount: 1 }},
+0);
+14.44 V/
